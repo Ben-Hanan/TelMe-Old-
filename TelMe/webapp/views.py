ac6499e6f10de63978417616fe9ac230a8597229
BlondeBubblyBryan <bryancwh98@gmail.com>
2020-05-19 20:06:01 +0800

    deleted redundant forloop

--- TelMe/webapp/views.py
+++ TelMe/webapp/views.py
@@ -2,21 +2,6 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Product
 
-plans = [
-    {
-        'product': 'Plan1',
-        'title': 'Singtel',
-        'price': '$50',
-        'contract': '2 years'
-    },
-    {
-        'product': 'Plan2',
-        'title': 'StarHub',
-        'price': '$40',
-        'contract': '1 year'
-    }
-]
-
 def home(request):
     template = 'webapp/home.html'
     return render(request, template)
