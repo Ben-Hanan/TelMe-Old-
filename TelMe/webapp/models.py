3cbdaa06bd8ad77eed9e5702f3a923bb931319ac
BlondeBubblyBryan <bryancwh98@gmail.com>
2020-05-27 22:11:42 +0800

    added like feature, reviews model and fixed pagination

--- TelMe/webapp/models.py
+++ TelMe/webapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Tag(models.Model):
@@ -20,14 +21,46 @@ class Product(models.Model):
         ('M1', 'M1'),
     )
 
+    CONTRACT = (
+        ('6 Months', '6 Months'),
+        ('1 Year', '1 Year'),
+        ('2 Years', '2 Years'),
+        ('More than 2 Years', 'More than 2 Years'),
+    )
     name = models.CharField(max_length=200, null=True)
     telco = models.CharField(max_length=200, null=True, choices=TELCO)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    contract = models.CharField(max_length=10, null=True)
+    contract = models.CharField(max_length=20, null=True, choices=CONTRACT)
     price = models.FloatField(null=True)
     description = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    tags = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag, blank=True)
+    liked = models.ManyToManyField(User, default=None, blank=True, related_name='liked')
 
     def __str__(self):
         return self.name
+    
+    @property
+    def num_likes(self):
+        return self.liked.all.count() 
+
+class Review(models.Model):
+    product = models.OneToOneField(Product, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author')
+    review = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Like(models.Model):
+    LIKE_CHOICES = (
+        ('Like', 'Like'),
+        ('Unlike', 'Unlike'),
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    value = models.CharField(choices=LIKE_CHOICES, default='Like', max_length=10)
+
+    def __str__(self):
+        return str(self.product)
--- TelMe/webapp/views.py
+++ TelMe/webapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
 from .filters import ProductFilter
 from django.core.paginator import Paginator
@@ -6,7 +6,6 @@ from django.core.paginator import Paginator
 def home(request):
     template = 'webapp/home.html'
     context = {
-
     }
     return render(request, template, context)
 
@@ -15,7 +14,7 @@ def products(request):
     products = Product.objects.all()
     myFilter = ProductFilter(request.GET, queryset=products)
     products = myFilter.qs
-    paginator = Paginator(products, 10)
+    paginator = Paginator(products, 4)
     page = request.GET.get('page')
     products = paginator.get_page(page)
 
@@ -25,3 +24,25 @@ def products(request):
     }
     
     return render(request, template, context)
+
+def like_product(request):
+    user = request.user
+    if request.method == 'POST':
+        product_id = request.POST.get('product_id')
+        product_obj = Product.objects.get(id=product_id)
+
+        if user in product_obj.liked.all():
+            product_obj.liked.remove(user)
+        else:
+            product_obj.liked.add(user)
+        
+        like, created = Like.objects.get_or_create(user=user, product_id=product_id)
+
+        if not created:
+            if like.value == 'Like':
+                like.value = 'Unlike'
+            else:
+                like.value = 'Like'
+        
+        like.save()
+    return redirect('products')
